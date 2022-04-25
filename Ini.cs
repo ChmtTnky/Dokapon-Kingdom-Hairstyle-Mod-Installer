1bb1b4c12de2d054c0055a87832b3bb7b1fc8b05
GLugia <glugia@hotmail.com>
2022-04-24 20:48:27 -0400

    Updated INI

--- Ini.cs
+++ Ini.cs
@@ -24,7 +24,7 @@ namespace DokaModInterface
 			{
 				if (!dict.ContainsKey(key))
 				{
-					dict.Add(key, value);
+					dict[key] = new();
 				}
 			}
 		}
@@ -33,19 +33,15 @@ namespace DokaModInterface
 		{
 			get
 			{
-				if (!dict.ContainsKey(key1))
+				if (!this[key1].ContainsKey(key2))
 				{
-					dict[key1] = new();
+					return null;
 				}
-				return dict[key1][key2];
+				return this[key1][key2];
 			}
 			set
 			{
-				if (!dict.ContainsKey(key1))
-				{
-					dict[key1] = new();
-				}
-				dict[key1].Add(key2, value);
+				this[key1][key2] = value;
 			}
 		}
 
@@ -69,7 +65,7 @@ namespace DokaModInterface
 				}
 				if (line.Contains('[') && line.Contains(']'))
 				{
-					section = line[1..^2].Trim();
+					section = line[1..^1].Trim();
 				}
 				if (section == null)
 				{
@@ -80,7 +76,7 @@ namespace DokaModInterface
 				{
 					continue;
 				}
-				ret[section][line_split[0].Trim().ToLowerInvariant()] = line_split[1].Trim();
+				ret[section][line_split[0].Trim()] = line_split[1].Trim();
 			}
 
 			return ret;
